306c49b1180aae2addd7c70e7baee3a108462b90
Shane <shanewilsonpro@gmail.com>
2020-06-27 20:30:50 -0700

    adding responsive and adaptive ui

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 import TitleText from "../components/TitleText";
-import Colors from '../constants/color';
+import Colors from "../constants/color";
 
-const Header = props => {
-    return (
-        <View style={styles.header}>
-            <TitleText>{props.title}</TitleText>
-        </View>
-    );
+const Header = (props) => {
+  return (
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
+      <TitleText style={styles.title}>{props.title}</TitleText>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: 70,
-        paddingTop: 20,
-        backgroundColor: '#f7287b',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  headerBase: {
+    width: "100%",
+    height: 70,
+    paddingTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerIOS: {
+    backgroundColor: "white",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
+  },
+  title: {
+    color: Platform.OS === "ios" ? Colors.primary : "white",
+  },
 });
 
 export default Header;
--- components/MainButton.android.js
+++ components/MainButton.android.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from "react-native";
 
 import Colors from "../constants/color";
 
 const MainButton = (props) => {
+  let ButtonComponent = TouchableOpacity;
+
+  if (Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+    <View style={styles.buttonContainer}>
+    <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
       <View style={styles.button}>
         <Text style={styles.buttonText}>{props.children}</Text>
       </View>
-    </TouchableOpacity>
+    </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
   button: {
       backgroundColor: Colors.primary,
       paddingVertical: 12,
